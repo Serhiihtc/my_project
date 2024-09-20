47d4616d9355827a9fa536dba651bb3edc77421c
Serhiihtc <sergeuhtc@gmail.com>
2024-09-20 17:30:10 +0300

    my first commit

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-print ( "Hello word" )
-
 nuber = int(input('Введите число: '))
 digit_1 = nuber % 10
 digit_2 = (nuber // 10) % 10
 digit_3 = (nuber // 100) % 10
 digit_4 = nuber // 1000
-print(digit_4,digit_3,digit_2,digit_1, sep = '\n')
+print(digit_4,digit_3,digit_2,digit_1, sep='\n')
+
 
 
 n1 = int(input('Введите число: '))
