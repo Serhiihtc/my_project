f892e1ab83599bb460b3b7cb020fee2c9047dfe3
Serhiihtc <sergeuhtc@gmail.com>
2024-09-20 13:34:01 +0300

    commit

--- test.py
+++ test.py
@@ -4,7 +4,14 @@ my_list = (1,2,3,4,5)
 for item in my_list:
     print(item)
 
- 
+n = int(input('Введите число: '))
+res = ""
+while n > 0:
+    res = str(n % 10)+ "" + res
+    n = n//10
+print (res.strip())
+
+
 
 n1 = int(input('Введите число: '))
 n2 = 0
