e5924686248df0a63a99c163dae56f252e1cdc5b
Serhiihtc <sergeuhtc@gmail.com>
2024-09-24 12:50:17 +0300

    commit

--- list.py
+++ list.py
@@ -22,5 +22,18 @@ while True:
             print(a * b)
         else:
             print("Деление на ноль!")
-    
 
+########Move an item in the list#########
+list = [3, 4, 5, 6, 7, 8, 9, 12]
+list.insert(0, list.pop())
+print("Исходный список : " + str(list))
+
+
+########
+
+lst = ['cow', 1, 'cat', 2, 'dog', 3]
+animals = []
+num = []
+for i in list:
+    num.append(i) if i.isnumeric() else animals.append(i)
+    
