119b8c4b4633b6b13552c5448d791ba2bed5ab58
Serhiihtc <sergeuhtc@gmail.com>
2024-09-24 22:13:12 +0300

    commint

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ list = [3, 4, 5, 6, 7, 8, 9, 12]
 list.insert(0, list.pop())
 print("Исходный список : " + str(list))
 
+########will split one list into two lists############
+
+arr = [[1, 2, 3, 4, 5, 6], [1, 2, 3], [1, 2, 3, 4, 5], [1], []]
+for a in arr:
+    i = len(a) - len(a) // 2
+    print(a, '=>', [a[:i], a[i:]])
+
+
+
+
