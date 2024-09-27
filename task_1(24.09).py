fb3a53f9ff5633fb87dfb6fa00051ae115aad564
Serhiihtc <sergeuhtc@gmail.com>
2024-09-27 17:50:09 +0300

    commit

---
+++ task_1(24.09).py
@@ -0,0 +1,25 @@
+####Calc####
+
+print("Для завершения операции введите 0 в качестве знака операции")
+
+while True:
+    a = float(input('Введите первое число '))
+    b = float(input('Введите второе число '))
+    s =input("Знак (+, -, *, /,): ")
+    if s == '0':
+        break
+    if s not in ('+', '-', '*', '/'):
+        continue
+
+    if s == '+':
+        print(a + b)
+    elif s == '-':
+        print(a - b)
+    elif s == '*':
+        print(a * b)
+    elif s == '/':
+        if b != 0:
+            print(a * b)
+        else:
+            print("Деление на ноль!")
+    
