facf967b28bcaf99adbc9ef990e25f69836e4ddb
Serhiihtc <sergeuhtc@gmail.com>
2024-10-04 14:35:30 +0300

    commit

---
+++ lesson5_task2_3.10.py
@@ -0,0 +1,24 @@
+while True:
+    x = float(input())
+    y = input()
+    z = float(input())
+
+    if y == '+':
+        print(x + z)
+    elif y == '-':
+        print(x - z)
+    elif y == '*':
+        print(x * z)
+    elif y == '/' and z == 0:
+        print("Делитель не равен 0")
+    elif y == '/':
+        print(x/z)
+
+    s = input('Считаем дальше? ')
+    if s == 'y' or  s == 'yes':
+        break
+    else:
+        print('Тогда считаем дальше')            
+
+        
+
