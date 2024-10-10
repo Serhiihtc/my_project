f5a0502039d8d8cb7c3201ab0cbef0421d8a1f11
Serhiihtc <sergeuhtc@gmail.com>
2024-10-10 20:54:14 +0300

    commit

--- lesson7_task1_10.10.py
+++ lesson7_task1_10.10.py
@@ -1,3 +1,8 @@
 def say_hi(name, age):
     return f"Привіт. Мене звати {name} і мені {age} років"
-print(say_hi("Сергій", 48))
+
+name = "Сергій"
+age = 48
+
+greeting = say_hi(name, age)
+print(greeting)
