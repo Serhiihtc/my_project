920765647d7302839cff6705708a763ece05a148
Serhiihtc <sergeuhtc@gmail.com>
2024-10-10 17:36:06 +0300

    commit

--- lesson7_task1_10.10.py
+++ lesson7_task1_10.10.py
@@ -1,3 +1,3 @@
 def say_hi(name, age):
-    return f"Hi. My name is {name} and I'm {age} yers old"
-print(say_hi("Bob", 43))
+    return f"Привіт. Мене звати {name} і мені {age} років"
+print(say_hi("Сергій", 48))
